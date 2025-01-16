a72d098ca84ae74e9c31619b7f7f158457f185f7
Park Jae Hyeon <pgs1835@naver.com>
2025-01-16 16:59:37 +0900

    feat: 초기세팅

---
+++ src/main/java/com/project/cook_mate/CookMateApplication.java
@@ -0,0 +1,13 @@
+package com.project.cook_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookMateApplication.class, args);
+	}
+
+}
