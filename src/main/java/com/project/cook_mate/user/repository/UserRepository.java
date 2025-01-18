aa23ae97146d5b0791f82a23a55761bcdf661ef4
Park Jae Hyeon <pgs1835@naver.com>
2025-01-18 17:19:53 +0900

    feat: id 중복확인

--- src/main/java/com/project/cook_mate/user/repository/UserRepository.java
+++ src/main/java/com/project/cook_mate/user/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.project.cook_mate.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByEmail(String email);
 }
